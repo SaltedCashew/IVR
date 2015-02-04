b048d7387f11dc7457413c04ee8e9c38a1628f33
SaltedCashew <bpgray@utexas.edu>
2015-02-04 16:53:39 +0000

    Quick change to colorDetect; added extra files

--- colorDetect.m
+++ colorDetect.m
@@ -1,4 +1,6 @@
 function color = colorDetect(originalImage, binaryImage)
+    close all; % Close all figures (except those of imtool.)
+    imtool close all; % Close all imtool figures.
     a = size(originalImage); %if this is in color, it will be a 3D array
     b = size(binaryImage);   %Should be a 2D binary image array
     c = originalImage;
@@ -10,8 +12,9 @@ function color = colorDetect(originalImage, binaryImage)
 
     columns = a(1,1);
     rows = a(1,2);
-    subplot(3,4,1)
+    figure;
     imshow(originalImage);
+     title('Original Image', 'FontSize', 10);
     for i=1:columns % pixel columns of image
         for j=1:rows % pixel rows
             if (binaryImage(i,j) == 1)
@@ -20,16 +23,16 @@ function color = colorDetect(originalImage, binaryImage)
         end
     end
     
-    hz = subplot(3,4,3);
+   figure;
     imshow(c);
-    
+    title('Binary Image', 'FontSize', 10);
     % Extract the individual color planes.
     redPlane = c(:, :, 1);
     greenPlane = c(:, :, 2);
     bluePlane = c(:, :, 3);
-    
+    figure;
     	% Compute and plot the red histogram. 
-	hR = subplot(3, 4, 5:8); 
+	hR = subplot(3, 4, 1:4); 
 	[countsR, grayLevelsR] = imhist(redPlane); 
 	maxGLValueR = find(countsR > 0, 1, 'last'); 
 	maxCountR = max(countsR); 
@@ -40,7 +43,7 @@ function color = colorDetect(originalImage, binaryImage)
 	title('Histogram of Red Band', 'FontSize', 10);
     
 % Compute and plot the green histogram. 
-	hG = subplot(3, 4, 9:12); 
+	hG = subplot(3, 4, 5:8); 
 	[countsG, grayLevelsG] = imhist(greenPlane); 
 	maxGLValueG = find(countsG > 0, 1, 'last'); 
 	maxCountG = max(countsG); 
@@ -50,10 +53,15 @@ function color = colorDetect(originalImage, binaryImage)
 	ylabel('Pixel Count'); 
 	title('Histogram of Green Band', 'FontSize', 10);
     
-        
-    if(grayLevelsG == grayLevelsR)
-        color = 'black';
-    else
-        color = 'red';
-	 
+% Compute and plot the blue histogram. 
+	hB = subplot(3, 4, 9:12); 
+	[countsB, grayLevelsB] = imhist(bluePlane); 
+	maxGLValueB = find(countsB > 0, 1, 'last'); 
+	maxCountB = max(countsB); 
+	bar(countsB, 'b'); 
+	grid on; 
+	xlabel('Gray Levels'); 
+	ylabel('Pixel Count'); 
+	title('Histogram of Blue Band', 'FontSize', 10);       
+  	 
 end
