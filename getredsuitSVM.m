799fa141d244a5124b1fec097640f8fb82d8ba2e
SaltedCashew <bpgray@utexas.edu>
2015-02-25 16:25:27 +0000

    Saved trained model data to features.mat for both colors

--- getredsuitSVM.m
+++ getredsuitSVM.m
@@ -1,9 +1,8 @@
 function prediction = getredsuitSVM(props)
 
+%Red_Pip_Hog_Features contains the features matrix, labels cell array, and trained svm multiclass model SVMModel  
 load Red_Pip_Hog_Features
-SVMModel = fitcecoc(features,labels);
-
 [label,score] = predict(SVMModel,props);
-
 prediction = label;
+
 end
